4d23368459d92e26fa82500ef8fdd322cd434f6e
geigea84 <geigea84@gmail.com>
2020-10-12 21:22:33 -0500

    added html class, outlined greentext plan for js dice roller

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/*creating a random number generator to create individual roll values 
+based on the values of each die type used and the total number of dice used
+
+thoughts: going to want to use math.floor and math.random
+
+vision: create dropdown for die type: d4, d6, d8, d10, d12, d20
+have +/- (and clear button to delete all) and number to display current 
+value at the top to request more or less dice, append dropdowns with 
+die values/remove dropdowns*/
+
