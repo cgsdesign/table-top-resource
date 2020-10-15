02f2e73aaf98f1b76184bce50424d4fa0a1ccfb1
uberbrent <bmjohns1337@gmail.com>
2020-10-15 15:57:51 -0500

    changed small elements in HTML and CSS to accomodate JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,10 @@
 var catagory = ""
 var entry = ""
 
-// var dndInfoCall = function() {
-//     fetch("https://www.dnd5eapi.co/api/" + catagory + "/" + entry).then(function(response) {
-//         response.json().then(function(data) {
-//             console.log(data)
-//         })
-//     })
-// }
+var fetchCatagory = function() {
 
-var createContextDrop = function() {
-    var rulesDropdownSelect = $("#rulesDropDown")
-    catagory = rulesDropdownSelect
-
-    fetch("https://www.dnd5eapi.co/api/" + catagory).then(function(response) {
-        response.json().then(function(data) {
-           console.log(data) 
-        })
-    })
 }
 
-$("#spellDrop").on("change", createContextDrop())
+$("#classBarb").on("click", function() {
+    fetchCatagory()
+})
