d3f1915d36ec76ae1fc3f5187734c2766ef1c229
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-10-16 17:57:28 -0400

    added psuedo code to know what to tackle next for js

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//able to select a time block and day and add it to table
+//saves name day and block to local storage
+//button to open the schedule module to schedule a block
+//able to clear your time or reschedule a day
