8e4dda9a6b3100dcd0f384665665e571fd298cd0
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-10-17 11:53:59 -0500

    feature images branch added, API pull in place

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,17 @@ var fetchCatagory = function() {
 $("#classBarb").on("click", function() {
     fetchCatagory()
 })
+
+
+
+
+//fetch pictures with PIXBAY
+var API_KEY = '18746508-966e8434b1d9d133d03bfb9ac';
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('fantasy');
+$.getJSON(URL, function(data){
+if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(hit.largeImageURL); });
+
+else
+    console.log('No hits');
+});
