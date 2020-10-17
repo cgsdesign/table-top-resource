42f1d579fa309b7c75d4ab40714c1905413ee755
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-10-17 11:39:10 -0400

    added select time and day dropdown, and some js logic to colsole.log the results of user selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,16 @@
 //saves name day and block to local storage
 //button to open the schedule module to schedule a block
 //able to clear your time or reschedule a day
+
+
+$(document).ready(function(){
+    $('#btnSubmit').click(function(){
+        var name = $('#name').val();
+        var time =$('#time').val();
+        console.log('Info', {
+            name: name,
+            time: time
+        })
+
+    })
+})
