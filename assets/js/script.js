0de89ec25cd4e4ee836c4f8394e165b57cd08857
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-10-17 16:21:17 -0500

    added a eleent to outside of pulled images so that I cound add href to link them to full sized page, added clear image search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,14 +28,20 @@ var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(`${se
                 for (var j=0; j < 4; j++){
                     //console.log(data.hits[j])
                     //console.log(data.hits[j].largeImageURL);
+                    //where images go
                     var placeImagesEl = document.querySelector('#placeImages');
-                    //searchImageInput.value = 'Search';
+                    //outer a to aply href
+                    var imgLinker = document.createElement("a");
+                    imgLinker.setAttribute('href', data.hits[j].largeImageURL );
+                    imgLinker.setAttribute('target', '_blank' );
+                    imgLinker.setAttribute('id', `${[j]}` );
+                    //inner img
                     var Img = document.createElement('img');
                     Img.setAttribute('src', data.hits[j].largeImageURL);
-                    Img.setAttribute('href', data.hits[j].largeImageURL );
-                    Img.setAttribute('url', "blank" );
                     console.log(Img)
-                    placeImagesEl.append(Img);
+                    //put it all together and drop in page
+                    imgLinker.append(Img);
+                    placeImagesEl.append(imgLinker);
                 }
                 return
             }
@@ -48,3 +54,12 @@ var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(`${se
 }
 var userFormEl = document.querySelector("#image-search-button");
 userFormEl.addEventListener("click", ImageSearch);
+
+//clear image search
+var clearImageSearch = function() {
+    var placeImagesEl = document.getElementById('placeImages')
+    placeImagesEl.innerHTML=""
+}
+
+var clearImageEl = document.querySelector("#image-search-clear");
+clearImageEl.addEventListener("click", clearImageSearch);
