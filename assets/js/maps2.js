3ed2e3a078dc53fb574eff5eee10f01d78747b0d
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-10-25 22:05:07 -0500

    outline for next step for maps

--- assets/js/maps2.js
+++ assets/js/maps2.js
@@ -1,14 +1,8 @@
-//nesisary Componants MVP
-    //print screen
-    //5 componants types w/ 3-5 of each to pull- water, rough terain, building, moster, player
-    //Players Monster Terain - needs to be in tabs/ not take up all the space-- can not do with print screen
-    //write in componants    
-    //for monster and player, need to be color coded, numbered, or have editable text
-//Componants to shoot for as have time
-    //auto spawning element of the same type when one is dragged--- need TA
-    //3 map sizes and hex option based fixed bottom option bar
-    // 1-4 prebuilt maps hosted on main page --- priority
-
+//next steps
+//find way to save images Node JS etc.??
+//change grid from image based to bg image change on click via class change via JS- 3 option clickable squares - taerrain options
+    //NOTES- keep player/monster pieces as dragable - 
+    //may try to make them possible via dropdown & sortable - snap to sortable each box? - NOTE boxes MUST be fixed height
 //drag
 $( function() {
     $( ".mymapdiv" ).draggable({
@@ -46,7 +40,7 @@ tokenNum = tokenNum +1
 return
 }
 
-//spon new boss monster to Drag-----------------------test Icon
+//spon new boss monster to Drag----------
 var bossTokenNum = 2
 var Trial = ""
 var createNewBossToken = function(){
@@ -76,175 +70,14 @@ return
 
 //  var bossMonster = document.getElementById("newBossMonster")
 //  bossMonster.addEventListener("click", createNewBossToken);
+//Convert blocks------------------------------------------------------------
 
-//spon new water to Drag---------------------------------------------------
-var waterTokenNum = 2
-var createNewWaterToken = function(){
-//make parent componant
-  for (i=0; i<10;i++){
-    var parent = document.getElementById("newWater")
-    var style = document.createElement('div');
-    style.classList.add("mymapdiv")
-    style.setAttribute("id", `mymapdivW${waterTokenNum}`)
-    style.innerHTML = `<img src="./assets/images/Tokens/water.png">`;
-    parent.appendChild(style);
-    //make dragable
-    $( function() {
-        $( ".mymapdiv" ).draggable({
-          containment: "document"
-        });
-        var someArray = [];
-      } );
-    //end
-    waterTrial = document.getElementById(`mymapdivW${waterTokenNum}`)
 
-    console.log(style)
-    console.log(waterTokenNum)
-    waterTokenNum = waterTokenNum +1
-  }
-  return
-}
-var WaterBG = document.getElementById("createNewWaterToken")
-WaterBG.addEventListener("click", createNewWaterToken);
 
-//spon new small water to Drag--------------------------------
-var waterTokenNumS = 2
-var createNewWaterTokenS = function(i){
-  //make parent componant
-  for (i=0; i<10;i++){
-    var parent = document.getElementById("newWaterS")
-    var style = document.createElement('div');
-    style.classList.add("mymapdiv")
-    style.setAttribute("id", `mymapdivw${waterTokenNumS}`)
-    style.innerHTML = `<img src="./assets/images/Tokens/water.png">`;
-    parent.appendChild(style);
-    //make dragable
-    $( function() {
-        $( ".mymapdiv" ).draggable({
-          containment: "document"
-        });
-        var someArray = [];
-      } );
-    //end
-    console.log(style)
-    console.log(waterTokenNumS)
-    waterTokenNumS = waterTokenNumS +1
-  }
-  return
-}
-var WaterS = document.getElementById("createNewWaterTokenS")
-WaterS.addEventListener("click", createNewWaterTokenS);
 
-//spon new wrough terain to Drag-----------------------------
-var wroughTokenNum = 2
-var createNewWroughToken = function(i){
-    for (i=0; i<10;i++){
-      //make parent componant
-      var parent = document.getElementById("newWrough")
-      var style = document.createElement('div');
-      style.classList.add("mymapdiv")
-      style.setAttribute("id", `mymapdivWr${wroughTokenNum}`)
-      style.innerHTML = `<img src="./assets/images/Tokens/wroughTerain.png">`;
-      parent.appendChild(style);
-      //make dragable
-      $( function() {
-          $( ".mymapdiv" ).draggable({
-            containment: "document"
-          });
-          var someArray = [];
-        } );
-      //end
-      console.log(style)
-      console.log(wroughTokenNum)
-      wroughTokenNum = wroughTokenNum +1
-    }
-  return
-}
-var WroughBG = document.getElementById("createNewWroughToken")
-WroughBG.addEventListener("click", createNewWroughToken);
-//spon new wrough terain SM to Drag------------------------
-var wroughTokenNumS = 2
-var createNewWroughTokenS = function(i){
-  for (i=0; i<10;i++){
-    //make parent componant
-    var parent = document.getElementById("newWroughS")
-    var style = document.createElement('div');
-    style.classList.add("mymapdiv")
-    style.setAttribute("id", `mymapdivwr${wroughTokenNumS}`)
-    style.innerHTML = `<img src="./assets/images/Tokens/wroughTerain.png">`;
-    parent.appendChild(style);
-    //make dragable
-    $( function() {
-        $( ".mymapdiv" ).draggable({
-          containment: "document"
-        });
-        var someArray = [];
-      } );
-    //end
-    console.log(style)
-    console.log(wroughTokenNumS)
-    wroughTokenNumS = wroughTokenNumS +1
-  }
-return
-}
-var WroughBG = document.getElementById("createNewWroughTokenS")
-WroughBG.addEventListener("click", createNewWroughTokenS);
-//spon new wall to Drag----------------------------------------------------
-var wallTokenNum = 2
-var createNewWallToken = function(i){
-  for (i=0; i<10;i++){
-      //make parent componant
-      console.log("spawn")
-    var parent = document.getElementById("newWall")
-    var style = document.createElement('div');
-    style.classList.add("mymapdiv")
-    style.setAttribute("id", `mymapdivWa${wallTokenNum}`)
-    style.innerHTML = `<img src="./assets/images/Tokens/wall.png">`;
-    parent.appendChild(style);
-    //make dragable
-    $( function() {
-        $( ".mymapdiv" ).draggable({
-          containment: "document"
-        });
-        var someArray = [];
-      } );
-    //end
-    console.log(style)
-    console.log(wallTokenNum)
-    wallTokenNum = wallTokenNum +1
-  }
-  return
-}
 
-var WallBG = document.getElementById("createNewWallToken")
-WallBG.addEventListener("click", createNewWallToken);
-//spon new wall SM to Drag-------------------------------------------------
-var wallTokenNumS = 2
-var createNewWallTokenS = function(i){
-    for (i=0; i<10;i++){
-      //make parent componant
-      var parent = document.getElementById("newWallS")
-      var style = document.createElement('div');
-      style.classList.add("mymapdiv")
-      style.setAttribute("id", `mymapdivwa${wallTokenNumS}`)
-      style.innerHTML = `<img src="./assets/images/Tokens/wall.png">`;
-      parent.appendChild(style);
-      //make dragable
-      $( function() {
-          $( ".mymapdiv" ).draggable({
-            containment: "document"
-          });
-          var someArray = [];
-        } );
-      //end
-      console.log(style)
-      console.log(wallTokenNumS)
-      wallTokenNumS = wallTokenNumS +1
-    }
-  return
-}
-var WallS = document.getElementById("createNewWallTokenS")
-WallS.addEventListener("click", createNewWallTokenS);
+
+
 //SAVE MAP------------------------------------------------------------
 var takeshot = function() { 
     let div = 
